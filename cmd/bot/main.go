183c158f0d5a934e70858c8b08d3a1da9784e256
zhenya <yauheni.turban@innowise-group.com>
2022-08-11 16:24:45 +0300

    basic structure

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Zhenya671/telegram-bot-exchangeRates/pkg/telegram"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+)
 
 func main() {
-	fmt.Println("hello world")
+	bot, err := tgbotapi.NewBotAPI("5558699531:AAHIaGPR4utxz4yz5WYtqUO5NWEQnHFuBVQ")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	telegramBot := telegram.NewBot(bot)
+	if err := telegramBot.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Zhenya671/telegram-bot-exchangeRates
 
 go 1.18
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
